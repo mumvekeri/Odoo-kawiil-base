a9b6eae709b934bae120a5ac943526a816383c8b
mumvekeri <mumvekerichidochashe@gmail.com>
2026-08-18 17:25:27 +0200

    Add kawiil_base submodule (branch 19.0)

---
+++ kawiil_financing/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Kawiil Financing",
+    "summary": "Streamlines the loan application process for dealerships.",
+    "category": "Kawiil/Custom Modules",
+    "version": "1.0.0",
+    "author": "ODOP Trainee",
+    "maintainer": "Chidochashe Mumvekeri",
+    "depends": ["base"],
+    "license": "OPL-1",
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "data": [],
+}
